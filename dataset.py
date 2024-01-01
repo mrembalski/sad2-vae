73791d5bd205c038893e8561cfb8a9bf8eb0753d
mrembalski <31540274+mrembalski@users.noreply.github.com>
2024-01-01 15:06:50 +0100

    Dataset

---
+++ dataset.py
@@ -0,0 +1,53 @@
+"""
+This file contains the class to load the SMDG dataset.
+"""
+import os
+
+import torch
+from torch.utils.data import TensorDataset
+
+from constants import NORMALIZED_DATASET
+
+class SMDGDataset(TensorDataset): # pylint: disable=too-few-public-methods
+    """
+    Simple class to load the SMDG dataset from the normalized tensors.
+    """
+    def __init__(self, split: str = 'train'):
+
+        # Check if stacked tensors already exist
+        if os.path.exists(f"{NORMALIZED_DATASET}/{split}/stacked_tensors.pt"):
+            tensors, classes = torch.load(f"{NORMALIZED_DATASET}/{split}/stacked_tensors.pt")
+        else:
+            tensors = []
+            classes = []
+
+            for tensor_filename in os.listdir(f"{NORMALIZED_DATASET}/{split}/0/"):
+                tensors.append(torch.load(f"{NORMALIZED_DATASET}/{split}/0/{tensor_filename}"))
+                classes.append(torch.tensor(0))
+
+            for tensor_filename in os.listdir(f"{NORMALIZED_DATASET}/{split}/1/"):
+                tensors.append(torch.load(f"{NORMALIZED_DATASET}/{split}/1/{tensor_filename}"))
+                classes.append(torch.tensor(1))
+
+            torch.save(
+                (
+                    torch.stack(tensors),
+                    torch.stack(classes)
+                ),
+                f"{NORMALIZED_DATASET}/{split}/stacked_tensors.pt"
+            )
+
+            if tensors[0].ndim == 2:
+                # If the tensors are 2D, add a channel dimension
+                tensors = [tensor.unsqueeze(0) for tensor in tensors]
+
+            tensors = torch.stack(tensors)
+            classes = torch.stack(classes)
+
+        self.tensors = (
+            tensors,
+            classes
+        )
+
+        print(f"Loaded {len(self.tensors[0])} tensors from {split} split.")
+        print(f"Tensor shape: {self.tensors[0].shape}")
