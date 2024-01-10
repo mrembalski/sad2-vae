ab634f96e27b602257054fcd18e2b051d912591c
mrembalski <31540274+mrembalski@users.noreply.github.com>
2024-01-10 14:56:16 +0100

    Small latent space refactor

--- model/latent_space.py
+++ model/latent_space.py
@@ -28,45 +28,36 @@ class LatentSpace(nn.Module):
         self.fc_mu = nn.Linear(self.flattened_size, dims[0])
         self.fc_logvar = nn.Linear(self.flattened_size, dims[0])
 
-        for i in range(len(dims) - 1):
-            self.add_module(
-                f"fc_mu_{i}",
-                nn.Linear(dims[i], dims[i + 1]),
-            )
-            self.add_module(
-                f"fc_logvar_{i}",
-                nn.Linear(dims[i], dims[i + 1]),
-            )
+
+        # Additional layers for hierarchical structure
+        self.additional_fc_mu = nn.ModuleList([nn.Linear(dims[i], dims[i + 1]) for i in range(len(dims) - 1)])
+        self.additional_fc_logvar = nn.ModuleList([nn.Linear(dims[i], dims[i + 1]) for i in range(len(dims) - 1)])
 
     def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
+        # Adding 1e-6 to logvar to avoid numerical instability
+        std = torch.exp(0.5 * logvar + 1e-6)
         eps = torch.randn_like(std)
         return eps * std + mu
 
     def forward(self, x):
-        # Return all the means and logvars
-        mus = []
-        logvars = []
-
-        # Instead of flattening, we use a global pooling layer
+        # Flatten the input for the linear layers
         x = torch.flatten(x, start_dim=1)
 
-        # Get the mean and logvar for the first layer
+        # First layer mean and logvar
         mu = self.fc_mu(x)
         logvar = self.fc_logvar(x)
 
-        # Append to the list
-        mus.append(mu)
-        logvars.append(logvar)
+        # Lists to store the hierarchical means and logvars
+        mus = [mu]
+        logvars = [logvar]
 
-        # Get the mean and logvar for the rest of the layers
+        # Process through additional layers
         for i in range(len(self.dims) - 1):
-            # pylint: disable=unnecessary-dunder-call
-            mu = self.__getattr__(f"fc_mu_{i}")(mu)
-            logvar = self.__getattr__(f"fc_logvar_{i}")(logvar)
+            mu = self.additional_fc_mu[i](mu)
+            logvar = self.additional_fc_logvar[i](logvar)
 
             mus.append(mu)
             logvars.append(logvar)
 
-        # Return the means and logvars
+        # Return the hierarchical means and logvars
         return mus, logvars
